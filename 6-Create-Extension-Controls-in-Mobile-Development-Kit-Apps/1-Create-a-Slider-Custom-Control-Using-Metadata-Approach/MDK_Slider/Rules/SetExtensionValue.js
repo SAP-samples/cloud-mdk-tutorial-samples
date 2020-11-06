c3e261637741e689bd1f6858df0a2719aeaec2be
Jitendra Kansal <jitendra.kansal@sap.com>
2020-11-06 12:11:44 +0100

    Updated projects

--- 6-Create-Extension-Controls-in-Mobile-Development-Kit-Apps/1-Create-a-Slider-Custom-Control-Using-Metadata-Approach/MDK_Slider/Rules/SetExtensionValue.js
+++ 6-Create-Extension-Controls-in-Mobile-Development-Kit-Apps/1-Create-a-Slider-Custom-Control-Using-Metadata-Approach/MDK_Slider/Rules/SetExtensionValue.js
@@ -1,6 +1,6 @@
-export default function SetExtensionValue(context) {
-    console.log("In SetExtensionValue");
-    let srcValue = context.getValue();
-    let targetCtrl = context.evaluateTargetPath("#Page:SliderExtension/#Control:MyExtensionControlName");
-    targetCtrl.setValue(srcValue);
-}
+    export default function SetExtensionValue(context) {
+        console.log("In SetExtensionValue");
+        let srcValue = context.getValue();
+        let targetCtrl = context.evaluateTargetPath("#Page:SliderExtension/#Control:MyExtensionControlName");
+        targetCtrl.setValue(srcValue);
+    }
