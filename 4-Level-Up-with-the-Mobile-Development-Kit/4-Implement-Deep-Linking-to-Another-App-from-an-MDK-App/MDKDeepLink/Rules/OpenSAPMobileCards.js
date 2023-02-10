aa58650a0eab3007d36bdf6d21bee14de7f4cc3a
Jitendra Kansal <jitendra.kansal@sap.com>
2023-02-10 07:08:55 +0530

    Fixed a rule

--- 4-Level-Up-with-the-Mobile-Development-Kit/4-Implement-Deep-Linking-to-Another-App-from-an-MDK-App/MDKDeepLink/Rules/OpenSAPMobileCards.js
+++ 4-Level-Up-with-the-Mobile-Development-Kit/4-Implement-Deep-Linking-to-Another-App-from-an-MDK-App/MDKDeepLink/Rules/OpenSAPMobileCards.js
@@ -1,10 +1,17 @@
 export default function OpenSAPMobileCards(context) {
     // Get the Nativescript Utils Module
     const utilsModule = context.nativescript.utilsModule;
+    // Get the Nativescript Platform Module
+    const platformModule = context.nativescript.platformModule;
     return context.executeAction('/MDKDeepLink/Actions/Confirmation.action').then((result) => {
         if (result.data) {
             //This will open SAP Mobile Cards app
-            return utilsModule.openUrl("com.sap.content2go://");
+            if (platformModule.isIOS) {
+                return utilsModule.openUrl("com.sap.content2go://");
+            } else if (platformModule.isAndroid) {
+                return utilsModule.openUrl("https://mobileservices.cloud.sap/mobilecards");
+            }
+
         } else {
             return Promise.reject('User Deferred');
         }
